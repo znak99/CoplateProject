7a81efd2693e3b24e252c4edd69a6d9fa8c1c619
znak99 <xhahekcldhkznak0113@gmail.com>
2023-01-29 22:53:54 +0900

    all-auth

--- coplate/models.py
+++ coplate/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from .validators import validate_no_special_characters
 
 class User(AbstractUser):
-    pass
+    nickname = models.CharField(
+        max_length=15, 
+        unique=True, 
+        null=True,
+        validators=[validate_no_special_characters],
+        error_messages={'unique': '이미 사용중인 닉네임입니다.'},
+    )
+    
+    def __str__(self):
+        return self.email
