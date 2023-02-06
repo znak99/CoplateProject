2a71a781ce79f3177612726cb8eff0ca58f1ed60
znak99 <xhahekcldhkznak0113@gmail.com>
2023-02-07 03:02:34 +0900

    design

--- CoplateProject/settings.py
+++ CoplateProject/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'coplate',
+    'widget_tweaks',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -129,6 +130,7 @@ ACCOUNT_PASSWORD_INPUT_RENDER_VALUE = True
 ACCOUNT_CONFIRM_EMAIL_ON_GET = True
 ACCOUNT_EMAIL_CONFIRMATION_AUTHENTICATED_REDIRECT_URL = "account_email_confirmation_done"
 ACCOUNT_EMAIL_CONFIRMATION_ANONYMOUS_REDIRECT_URL = "account_email_confirmation_done"
+ACCOUNT_EMAIL_SUBJECT_PREFIX = ""
 
 # Email settings
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
--- CoplateProject/urls.py
+++ CoplateProject/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     # allauth
     path(
         'email-confirmation-done/',
-        TemplateView.as_view(template_name="coplate/email_confirmation_done.html"),
+        TemplateView.as_view(template_name='account/email_confirmation_done.html'),
         name='account_email_confirmation_done',
     ),
     path('password/change/', CustomPasswordChangeView.as_view(), name="account_password_change"),
