ff7815bd74336322d4a8a8fcfe55fd679c45e3d8
znak99 <xhahekcldhkznak0113@gmail.com>
2023-02-26 02:47:35 +0900

    Access Control

--- coplate/views.py
+++ coplate/views.py
@@ -7,9 +7,13 @@ from django.views.generic import (
     UpdateView,
     DeleteView,
 )
+from braces.views import LoginRequiredMixin, UserPassesTestMixin
+from allauth.account.models import EmailAddress
 from allauth.account.views import PasswordChangeView
 from coplate.models import Review
 from coplate.forms import ReviewForm
+from coplate.functions import confirmation_required_redirect
+
 class IndexView(ListView):
     model = Review
     template_name = "coplate/index.html"
@@ -17,15 +21,18 @@ class IndexView(ListView):
     paginate_by = 4
     ordering = ["-dt_created"]
     
+
 class ReviewDetailView(DetailView):
     model = Review
     template_name = "coplate/review_detail.html"
     pk_url_kwarg = "review_id"
     
-class ReviewCreateView(CreateView):
+class ReviewCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Review
     form_class = ReviewForm
     template_name = "coplate/review_form.html"
+    redirect_unauthenticated_users = True
+    raise_exception = confirmation_required_redirect
     
     def form_valid(self, form):
         form.instance.author = self.request.user
@@ -34,22 +41,39 @@ class ReviewCreateView(CreateView):
     def get_success_url(self):
         return reverse("review-detail", kwargs={"review_id": self.object.id})
     
-class ReviewUpdateView(UpdateView):
+    def test_func(self, user):
+        return EmailAddress.objects.filter(user=user, verified=True).exists()
+    
+    
+
+class ReviewUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Review
     form_class = ReviewForm
     template_name = "coplate/review_form.html"
     pk_url_kwarg = "review_id"
     
+    raise_exception = True
+    
     def get_success_url(self):
         return reverse("review-detail", kwargs={"review_id": self.object.id})
+    
+    def test_func(self, user):
+        review = self.get_object()
+        return review.author == user
 
-class ReviewDeleteView(DeleteView):
+class ReviewDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Review
     template_name = "coplate/review_confirm_delete.html"
     pk_url_kwarg = "review_id"
     
+    raise_exception = True
+    
     def get_success_url(self):
         return reverse("index")
+    
+    def test_func(self, user):
+        review = self.get_object()
+        return review.author == user
 
 class CustomPasswordChangeView(PasswordChangeView):
     def get_success_url(self):
